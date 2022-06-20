04484c7a520471874d24e46056a0b711a54b8af6
nelson-eduardo <nelsoneduardo@sapo.ao>
2022-06-20 13:33:02 +0100

    "correcao"

--- main.py
+++ main.py
@@ -40,28 +40,28 @@ with dataset:
     st.write(df_Movimentos.head(5))
     st.subheader('Legenda para onosso grafico...!')
 
-    sel_col, disp_col = st.columns(2)
-    max1 = sel_col.slider("Qual seria o valor para a profundidade", min_value= 0, max_value= 100, step=1, value=0)
-    teste = pd.DataFrame(df_Movimentos['block_id'].value_counts()).head(max1)
-    # st.bar_chart(teste)
+#     sel_col, disp_col = st.columns(2)
+#     max1 = sel_col.slider("Qual seria o valor para a profundidade", min_value= 0, max_value= 100, step=1, value=0)
+#     teste = pd.DataFrame(df_Movimentos['block_id'].value_counts()).head(max1)
+#     # st.bar_chart(teste)
 
-with feature:
-    st.header("APlicacao sobre os dados de eleicao geral em Angola 2022")
-    st.text("Neste projecto,desenvolvo um projecto de python com a platoforma Streamlit...")
+# with feature:
+#     st.header("APlicacao sobre os dados de eleicao geral em Angola 2022")
+#     st.text("Neste projecto,desenvolvo um projecto de python com a platoforma Streamlit...")
 
-with model_training:
-    st.header("APlicacao sobre os dados de eleicao geral em Angola 2022")
-    # st.button("botao para download")
-    # st.camera_input("Sorrir! Estas a ser filmado!")
-    st.text("Neste projecto,desenvolvo um projecto de python com a platoforma Streamlit...")
+# with model_training:
+#     st.header("APlicacao sobre os dados de eleicao geral em Angola 2022")
+#     # st.button("botao para download")
+#     # st.camera_input("Sorrir! Estas a ser filmado!")
+#     st.text("Neste projecto,desenvolvo um projecto de python com a platoforma Streamlit...")
 
 
-# Novos testes
-sel_col1, disp_col = st.columns(2)
-# max_diph = sel_col.slider("Qual seria o valor para a profundidade", min_value= 10, max_value= 100, step=1, value=0)
+# # Novos testes
+# sel_col1, disp_col = st.columns(2)
+# # max_diph = sel_col.slider("Qual seria o valor para a profundidade", min_value= 10, max_value= 100, step=1, value=0)
 
-bloco_selecao = sel_col.selectbox("Seleciona uma das opcoes",["Homem", "Mulher", "Nenhum"])
-entrada = st.text_input("Digita qualquer coisa", 'categorias')
+# bloco_selecao = sel_col.selectbox("Seleciona uma das opcoes",["Homem", "Mulher", "Nenhum"])
+# entrada = st.text_input("Digita qualquer coisa", 'categorias')
 
-# st.write(max_diph)
-st.write(bloco_selecao)
+# # st.write(max_diph)
+# st.write(bloco_selecao)
